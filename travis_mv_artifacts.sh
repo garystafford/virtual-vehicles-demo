9b29502ce3e8cc15f2385b88dcb6fea51c3887d7
Gary A. Stafford <garystafford@rochester.rr.com>
2015-08-05 20:22:46 -0400

    Testing new Travis CI configuration

---
+++ travis_mv_artifacts.sh
@@ -0,0 +1,16 @@
+mkdir -p artifacts/authentication/
+mkdir -p artifacts/maintenance/
+mkdir -p artifacts/valet/
+mkdir -p artifacts/vehicle/
+
+mv Authentication/target/Authentication-1.0-SNAPSHOT.jar 	artifacts/authentication/Authentication-1.${TRAVIS_BUILD_NUMBER}.jar
+mv Authentication/target/config/test/environment.properties	artifacts/authentication/environment.properties
+
+mv Maintenance/target/Maintenance-1.0-SNAPSHOT.jar 			artifacts/maintenance/Maintenance-1.${TRAVIS_BUILD_NUMBER}.jar
+mv Maintenance/target/config/test/environment.properties 	artifacts/maintenance/environment.properties
+
+mv Valet/target/Valet-1.0-SNAPSHOT.jar 						artifacts/valet/Valet-1.${TRAVIS_BUILD_NUMBER}.jar
+mv Valet/target/config/test/environment.properties 			artifacts/valet/environment.properties
+
+mv Vehicle/target/Vehicle-1.0-SNAPSHOT.jar 					artifacts/vehicles/Vehicle-1.${TRAVIS_BUILD_NUMBER}.jar
+mv Vehicle/target/config/test/environment.properties 		artifacts/vehicles/environment.properties

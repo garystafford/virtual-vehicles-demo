55ffd1c34ca1c03566a2af498952091484bcdfd6
Gary A. Stafford <garystafford@rochester.rr.com>
2015-08-05 23:04:18 -0400

    Testing new Travis CI configuration

--- travis_mv_artifacts.sh
+++ travis_mv_artifacts.sh
@@ -5,14 +5,14 @@ mkdir -p artifacts/maintenance/
 mkdir -p artifacts/valet/
 mkdir -p artifacts/vehicle/
 
-cp Authentication/target/Authentication-1.0-SNAPSHOT.jar    artifacts/authentication/Authentication-1.${TRAVIS_BUILD_NUMBER}.jar
-cp Authentication/target/config/test/environment.properties artifacts/authentication/environment.properties
+cp Authentication/target/Authentication-1.0-SNAPSHOT.jar    artifacts/authentication/
+cp Authentication/target/config/test/environment.properties artifacts/authentication/
 
-cp Maintenance/target/Maintenance-1.0-SNAPSHOT.jar  artifacts/maintenance/Maintenance-1.${TRAVIS_BUILD_NUMBER}.jar
-cp Maintenance/target/config/test/environment.properties    artifacts/maintenance/environment.properties
+cp Maintenance/target/Maintenance-1.0-SNAPSHOT.jar  artifacts/maintenance/
+cp Maintenance/target/config/test/environment.properties    artifacts/maintenance/
 
-cp Valet/target/Valet-1.0-SNAPSHOT.jar  artifacts/valet/Valet-1.${TRAVIS_BUILD_NUMBER}.jar
-cp Valet/target/config/test/environment.properties  artifacts/valet/environment.properties
+cp Valet/target/Valet-1.0-SNAPSHOT.jar  artifacts/valet/
+cp Valet/target/config/test/environment.properties  artifacts/valet/
 
-cp Vehicle/target/Vehicle-1.0-SNAPSHOT.jar  artifacts/vehicle/Vehicle-1.${TRAVIS_BUILD_NUMBER}.jar
-cp Vehicle/target/config/test/environment.properties    artifacts/vehicle/environment.properties
+cp Vehicle/target/Vehicle-1.0-SNAPSHOT.jar  artifacts/vehicle/
+cp Vehicle/target/config/test/environment.properties    artifacts/vehicle/
